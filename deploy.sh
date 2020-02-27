970c5e199392ff6043a5d25d42da6aa135893daa
Q356797 <q356797@JBHATTACHARYA.local>
2020-02-27 14:22:28 -0800

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-docker build -t jbhatta/multi-client -f ./client/Dockerfile ./client
-docker build -t jbhatta/multi-server -f ./server/Dockerfile ./server
-docker build -t jbhatta/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t jbhatta/multi-client:latest -t jbhatta/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jbhatta/multi-server:latest -t jbhatta/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jbhatta/multi-worker:latest -t jbhatta/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push jbhatta/multi-client
-docker push jbhatta/multi-server
-docker push jbhatta/multi-worker
+docker push jbhatta/multi-client:latest
+docker push jbhatta/multi-server:latest
+docker push jbhatta/multi-worker:latest
+
+docker push jbhatta/multi-client:$SHA
+docker push jbhatta/multi-server:$SHA
+docker push jbhatta/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=jbhatta/multi-server
+kubectl set image deployments/server-deployment server=jbhatta/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jbhatta/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jbhatta/multi-worker:$SHA
